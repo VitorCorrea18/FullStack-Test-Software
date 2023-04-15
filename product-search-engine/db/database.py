cdda11309a0a2110f614cd28c2540063ba9c2775
VitorCorrea18 <vitorcorrea18@gmail.com>
2023-04-15 15:07:16 -0300

    Revert "feat: configure mongo connection"
    This reverts commit 321c3329ee790bc486e62a08852555bdf356b978.

--- product-search-engine/db/database.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from pymongo.mongo_client import MongoClient
-
-uri = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/search-engine')
-
-# Create a new client and connect to the server
-client = MongoClient(uri)
